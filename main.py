9b70c8db33d1131022ea42625a3e956f476bed5e
FESL Sapphire Dev <codspecialops@gmail.com>
2023-02-01 18:53:11 -0600

    Added footers

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from fpdf import FPDF as FP
 
 pdf = FP(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
 data = pd.read_csv("topics.csv")
 for index, row in data.iterrows():
 	pdf.add_page()
@@ -9,5 +11,16 @@ for index, row in data.iterrows():
 	pdf.set_text_color(100, 100, 101)
 	pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
 	pdf.line(10, 21, 200, 21)
+	pdf.ln(265)
+	pdf.set_font(family="Times", style="I", size=8)
+	pdf.set_text_color(180, 180, 180)
+	pdf.cell(w=0, h=10, txt=row["Topic"], align="R")\
+
+	for item in range(row["Pages"] - 1):
+		pdf.add_page()
+		pdf.ln(277)
+		pdf.set_font(family="Times", style="I", size=8)
+		pdf.set_text_color(180, 180, 180)
+		pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output(rf"notebook.pdf")
